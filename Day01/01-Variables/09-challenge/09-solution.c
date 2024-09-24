0f84cb7b8c00c8452a396e055d760de3644dc894
protocol <oujaberousama@gmail.com>
2024-09-24 20:17:29 +0200

    Day 01

--- Day01/01-Variables/09-challenge/09-solution.c
+++ Day01/01-Variables/09-challenge/09-solution.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     int x1, x2;
@@ -23,5 +24,14 @@ int main(){
     printf("Enter nbr: ");
     scanf("%d", &z2);
 
-    
+    int sub1, sub2, sub3;
+    float distance;
+
+    sub1 = x1 - x2;
+    sub2 = y1 - y2;
+    sub3 = z1 - z2;
+
+    distance = sqrt(pow(sub1, 2) + pow(sub2, 2) + pow(sub3, 2));
+
+    printf("ur Distance: %f\n", distance);
 }
