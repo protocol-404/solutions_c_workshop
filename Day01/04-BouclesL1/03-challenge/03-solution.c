c97225331950d7c2f66dbfaf8cd23a61b39333b2
protocol <oujaberousama@gmail.com>
2024-09-27 09:25:55 +0200

    Day 01

--- Day01/04-BouclesL1/03-challenge/03-solution.c
+++ Day01/04-BouclesL1/03-challenge/03-solution.c
@@ -1,5 +1,3 @@
-// Écrivez un programme C qui demande un nombre entier n à l’utilisateur et calcule la somme des n premiers nombres naturels. Par exemple, pour n = 4, la somme est 1 + 2 + 3 + 4 = 10.
-
 #include <stdio.h>
 
 int main() {
