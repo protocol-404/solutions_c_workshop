dfedaac1cfb73663fd6c9124405cf2ad3b85c37f
ooujaber <mail.ciom>
2024-09-28 00:10:52 +0100

    make it easy

--- Day01/01-Variables/13-challenge/13-solution.c
+++ Day01/01-Variables/13-challenge/13-solution.c
@@ -2,24 +2,29 @@
 
 int main() {
     int binaryNum[32];
-    int num;
-    
+    int num, n, i, j;
+
     printf("Enter an integer: ");
     scanf("%d", &num);
 
     printf("Hexadecimal: %X\n", num);
     printf("Binary: ");
-    
-    int n = num;
-    int i = 0;
+
+    n = num;
+    i = 0;
+
     while (n > 0) {
         binaryNum[i] = n % 2;
         n = n / 2;
         i++;
     }
-    for (int j = i - 1; j >= 0; j--)
+
+    j = i - 1;
+    while (j >= 0) {
         printf("%d", binaryNum[j]);
-    
+        j--;
+    }
+
     printf("\n");
 
     return 0;
